cb60412931c1113374d40bae63727bd040c168bd
SmolPotato <alan.gubric@metis.academy>
2026-02-27 21:36:00 +0100

    fih3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public CharacterController characterController;
+    public float rotateSpeed = 10f;
 
     public float backwardsSpeedDivide;
 
@@ -19,7 +20,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 move = this.transform.TransformDirection(moveInput);
         characterController.Move(moveInput * moveSpeed * Time.deltaTime);
+        this.transform.Rotate(rotateInput * rotateSpeed * Time.deltaTime);
+
     }
 
     public void OnMove(InputAction.CallbackContext context) {
@@ -32,6 +36,6 @@ public class PlayerMovement : MonoBehaviour
     }
 
     public void OnRotate(InputAction.CallbackContext context) {
-
+        rotateInput = context.ReadValue<Vector2>();
     }
 }
