f405aad9e94ed43be0fbf46a3ff753650e5736cd
SmolPotato <alan.gubric@metis.academy>
2026-02-28 11:46:44 +0100

    sasaassa

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,10 +7,15 @@ public class GameManager : MonoBehaviour
     public List<UpgradeCard> cards;
     public float moveSpeed = 10f;
     public float sharkSize = 40f;
+    public float score = 0f;
 
     
 
     public void increaseSpeed(float by) {
         moveSpeed += by;
     }
+
+    public void decreaseSpeed(float by) { moveSpeed -= by; }
+    public void addScore(float by) { score += by; }
+
 }
---
+++ Assets/Scripts/PickedItem.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PickedItem : MonoBehaviour
+{
+    public float points;
+    GameManager gameManager;
+    void Start()
+    {
+        gameManager = GameObject.FindWithTag("GameController").gameObject.GetComponent<GameManager>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("SSS");
+        if (collision == null) { return; }
+        if (collision.tag == "PlayerCollector") {
+            gameManager.addScore(points);
+            collision.transform.parent.GetComponent<PlayerMovement>().UpdateStats();
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
