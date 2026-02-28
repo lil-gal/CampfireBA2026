7ddfd540aafeb68212e110ae535f0c98dc48bcbc
martinlehocky <martin.lehocky2007@gmail.com>
2026-02-28 16:06:01 +0100

    Bug fixed.

--- Assets/Scripts/CameraTargetScript.cs
+++ Assets/Scripts/CameraTargetScript.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
 
-public class CameraTargetScript : MonoBehaviour {
+public class CameraTargetScript : MonoBehaviour
+{
     private Transform player_transform;
-    private float progress_x; // will only increment. player cannot go backwards
-    void Start() {
+
+    void Start()
+    {
         player_transform = transform.parent;
-        progress_x = player_transform.position.x;
     }
-    void Update() {
+
+    void Update()
+    {
         var pos = player_transform.position;
-        pos.x = Mathf.Max(progress_x, player_transform.position.x);
         pos.y = Mathf.Min(0, player_transform.position.y);
         transform.position = pos;
-
-        progress_x = pos.x;
     }
 }
--- Assets/Scripts/ParalaxBG.cs
+++ Assets/Scripts/ParalaxBG.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class ParalaxBG : MonoBehaviour {
     public Transform CameraTransform;
     public float YPositionMultiply = .75f;
+    public float XPositionMultiply = .75f;
 
     void Start() {
         if (CameraTransform == null)
             CameraTransform = FindFirstObjectByType<Camera>().transform;
     }
+
     void Update() {
         var pos = transform.localPosition;
-        pos.y = -CameraTransform.position.y*YPositionMultiply;
+        pos.x = CameraTransform.position.x * XPositionMultiply;
+        pos.y = CameraTransform.position.y * YPositionMultiply;
         transform.localPosition = pos;
     }
 }
