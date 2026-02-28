4edf7ac07e7f567989a8319807bb291838c77116
SmolPotato <alan.gubric@metis.academy>
2026-02-28 08:40:30 +0100

    sash

--- Assets/Scripts/JellyFishScript.cs
+++ Assets/Scripts/JellyFishScript.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class JellyFishScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float minBounceDistance = 1f;
+    public float minTimeToBounce = 2f;
+    public float maxBounceDistance = 8f;
+    public float maxTimeToBounce = 10f;
+
+    float bounceDistance;
+    float timeToBounce;
+
+    float timer;
+    bool up = false;
+
+    Rigidbody2D rb;
     void Start()
     {
-        
+        bounceDistance = Random.Range(minBounceDistance, maxBounceDistance);
+        timeToBounce = Random.Range(minTimeToBounce, maxTimeToBounce);
+        timer = timeToBounce;
+        rb = GetComponent<Rigidbody2D>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer > 0) {
+            int times = 1;
+
+            if (!up) {
+                times = -1;
+            }
+            
+            rb.linearVelocityY = times * Time.deltaTime * 100 * (bounceDistance/timeToBounce);
+
+            
+
+            timer-=Time.deltaTime;
+        } else {
+            timer = timeToBounce;
+            up = !up;
+        }
     }
 }
