3327f2a28811226357b8bdeab87639cb5de744d9
SmolPotato <alan.gubric@metis.academy>
2026-02-28 12:43:23 +0100

    MEOW

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
